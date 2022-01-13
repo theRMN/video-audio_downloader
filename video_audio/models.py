0b04a9f54ed01b1731997157287b2340639ff94a
theRMN <roman.vassev@gmail.com>
2022-01-13 13:33:56 +0600

    increase in max_lenght to 1000 from URLFields

--- video_audio/models.py
+++ video_audio/models.py
@@ -6,10 +6,10 @@ class VideoInfo(models.Model):
     title = models.TextField()
     length = models.TextField()
     resolution = models.TextField()
-    video_url = models.URLField()
-    audio_url = models.URLField()
+    video_url = models.URLField(max_length=1000)
+    audio_url = models.URLField(max_length=1000)
     abr = models.TextField()
-    image = models.URLField()
+    image = models.URLField(max_length=1000)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
